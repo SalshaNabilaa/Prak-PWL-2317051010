72087aff29d7bdd854f888df95785d371b8d9103
SalshaNabilaa <rosihoki17@gmail.com>
2025-10-15 15:25:32 +0700

    Selesai Langkah Praktikum Modul 6

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Aplikasi User</a>
+        <a class="navbar-brand fw-semibold" href="/">Aplikasi User</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
                 data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
                 aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="/user">List User</a></li>
-                <li class="nav-item"><a class="nav-link" href="/user/create">Tambah User</a></li>
+
+                {{-- Menu User --}}
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('user') ? 'fw-bold text-light' : '' }}" href="/user">
+                        List User
+                    </a>
+                </li>
+
+                {{-- Menu Mata Kuliah --}}
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('matakuliah') ? 'fw-bold text-light' : '' }}" href="/matakuliah">
+                        List Mata Kuliah
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
